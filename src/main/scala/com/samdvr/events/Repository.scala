76e884aa49ee8a218835cfed89a5a3f5e2a2de72
Sam Davarnia <>
2018-07-21 08:36:12 -0700

    refactor

--- src/main/scala/com/samdvr/events/Repository.scala
+++ src/main/scala/com/samdvr/events/Repository.scala
@@ -1,14 +1,14 @@
 package com.samdvr.events
 
 import java.sql.Timestamp
+import java.time.Instant
 import java.util.UUID
-
 
 import com.datastax.driver.core._
 
 import scala.concurrent.ExecutionContext
 
-case class Event(id: UUID, userID: UUID, timestamp: String, body: String)
+case class Event(userID: UUID, timestamp: String, body: String)
 
 import scala.collection.JavaConverters._
 class Repository {
@@ -22,14 +22,15 @@ class Repository {
   def save(event: Event) = {
     session.executeAsync(s"INSERT INTO events.Events(user_id, timestamp, body, event_id) " +
       s"VALUES (?,?,?,?)",
-      event.userID, Timestamp.valueOf(event.timestamp), event.body, event.id)
+      event.userID, Timestamp.from(Instant.parse(event.timestamp)), event.body, UUID.randomUUID())
   }
 
   def fetch(userID: String, from: String, to: String): Seq[Event] = {
     session.execute(s"SELECT * FROM events.Events WHERE user_id = ? " +
-      s"AND timestamp > ? AND timestamp < ?", UUID.fromString(userID), Timestamp.valueOf(from), Timestamp.valueOf(to)).all.asScala.map { row =>
+      s"AND timestamp > ? AND timestamp < ?",
+      UUID.fromString(userID), Timestamp.valueOf(from),
+      Timestamp.valueOf(to)).all.asScala.map { row =>
       Event(row.getUUID("user_id"),
-        row.getUUID("event_id"),
         row.getTimestamp("timestamp").toString,
         row.getString("body")
       )
