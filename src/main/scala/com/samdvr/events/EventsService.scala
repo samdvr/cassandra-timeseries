df22a6223be4dd0f695b20f148d07bcd9b3146db
Sam Davarnia <>
2018-07-21 07:23:26 -0700

    remove akka

--- src/main/scala/com/samdvr/events/EventsService.scala
+++ src/main/scala/com/samdvr/events/EventsService.scala
@@ -1,18 +1,34 @@
 package com.samdvr.events
 
 import cats.effect.{Effect, IO}
+import com.datastax.driver.core.Row
+import com.samdvr.events.EventsService.OptionalQueryParamDecoderMatcher
+import io.circe.Encoder
 import org.http4s.HttpService
 import org.http4s.dsl.Http4sDsl
 import io.circe.generic.auto._
 import org.http4s.circe._
+import org.http4s.circe._
+import org.http4s.dsl.impl.QueryParamDecoderMatcher
 
 import scala.concurrent.ExecutionContext
 
+object fromTime extends QueryParamDecoderMatcher[String]("from_time")
+
+object toTime extends QueryParamDecoderMatcher[String]("to_time")
+
 object EventsService extends Http4sDsl[IO] {
   implicit val decoder = jsonOf[IO, Event]
+  implicit val encoder = jsonEncoderOf[IO, Seq[Event]]
+
   val repo = new Repository
   val service: HttpService[IO] = {
     HttpService[IO] {
+
+      case GET -> Root / "events" / userID :? fromTime(fromTimestamp) +& toTime(toTimestamp)
+      => Ok(repo.fetch(userID, fromTimestamp, toTimestamp))
+
+
       case req@POST -> Root / "events" =>
         for {
           // Decode a Event request
--- src/main/scala/com/samdvr/events/Repository.scala
+++ src/main/scala/com/samdvr/events/Repository.scala
@@ -3,36 +3,37 @@ package com.samdvr.events
 import java.sql.Timestamp
 import java.util.UUID
 
-import akka.NotUsed
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.alpakka.cassandra.scaladsl.CassandraSink
-import akka.stream.scaladsl.{Keep, Source}
-import com.datastax.driver.core.{Cluster, PreparedStatement}
+
+import com.datastax.driver.core._
 
 import scala.concurrent.ExecutionContext
 
 case class Event(id: UUID, userID: UUID, timestamp: String, body: String)
 
-
+import scala.collection.JavaConverters._
 class Repository {
-  implicit val system: ActorSystem = ActorSystem("system")
-  implicit val mat: ActorMaterializer = ActorMaterializer()
-  implicit val ec: ExecutionContext = system.dispatcher
   implicit val session = Cluster.builder
     .addContactPoint("127.0.0.1")
     .withPort(9042)
     .build
     .connect()
-  val preparedStatement = session.prepare(s"INSERT INTO events.Events(user_id, timestamp, body, event_id) VALUES (?, ?,?,?)")
+
 
   def save(event: Event) = {
-    val source: Source[Event, NotUsed] = Source.single(event)
-    val binder = (event: Event, statement: PreparedStatement) => statement.bind(event.userID, Timestamp.valueOf(event.timestamp), event.body, event.id)
+    session.executeAsync(s"INSERT INTO events.Events(user_id, timestamp, body, event_id) " +
+      s"VALUES (?,?,?,?)",
+      event.userID, Timestamp.valueOf(event.timestamp), event.body, event.id)
+  }
 
-    val sink = CassandraSink[Event](parallelism = 2, preparedStatement, binder)
-    source.toMat(sink)(Keep.right)
-      .run()
+  def fetch(userID: String, from: String, to: String): Seq[Event] = {
+    session.execute(s"SELECT * FROM events.Events WHERE user_id = ? " +
+      s"AND timestamp > ? AND timestamp < ?", UUID.fromString(userID), Timestamp.valueOf(from), Timestamp.valueOf(to)).all.asScala.map { row =>
+      Event(row.getUUID("user_id"),
+        row.getUUID("event_id"),
+        row.getTimestamp("timestamp").toString,
+        row.getString("body")
+      )
+    }
 
 
   }
